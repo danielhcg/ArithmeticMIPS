f46604c70bafbbaf9aee91b4d6ed51311e1c60b1
danielhcg <danihcg.23@gmail.com>
2020-07-17 20:03:19 -0700

    Added dynamic iteration of while loop through user input

--- functionExample.asm
+++ functionExample.asm
@@ -1,23 +1,59 @@
+#need to do
+	#Done. ask user how many triangles to calculate
+	#Done. associate name with triangle and print out
+	#Done. display at each iteration which triangle we are currently working on
+	#Done. change title to include the number of iterations
+
+
 .data
 
-	areaPrompt: .asciiz "Calculate the area of 3 rectangles\n"
+	recNumPrompt: .asciiz "Enter number of rectangles you wish to process: "
+	areaPrompt1:  .asciiz "\nCalculating the area of "
+	areaPrompt2:  .asciiz " rectanlges.\n"
+	recNumber:    .asciiz "\nRectangle "
 	lengthPrompt: .asciiz "\nLength: "
-	widthPrompt: .asciiz "Width: "
-	areaOutPut: .asciiz "Area: "
-	termMessage: .asciiz "\nDone!"
-	newLine: .asciiz "\n"
+	widthPrompt:  .asciiz "Width: "
+	areaOutPut:   .asciiz "Area: "
+	termMessage:  .asciiz "\nDone!"
+	newLine:      .asciiz "\n"
 	
 
 .text
 
 	main: 
+		
+		#prompting user to enter number of rectangles they wish to 
+		#process
+		jal displayRecPrompt
+		#reading number of rectangles user wishes to process
+		li $v0, 5
+		syscall 
+		#moving user input from v0 to t1
+		move $t1, $v0
 	
-		jal displayTitle
+		#displaying first part of title
+		jal displayTitle1
+		#print number of triangles user wishes to process
+		li $v0, 1
+		move $a0, $t1
+		syscall
+		#displaying second part of title
+		jal displayTitle2
+		
 	
 		addi $t0, $zero, 0 #Incrementor
 		while:
 		
-			beq $t0, 3, exit
+			beq $t0, $t1, exit
+			
+				#displaying rectangle
+				li $v0, 4
+				la $a0, recNumber
+				syscall
+				#displaying rectangle number
+				li $v0, 1
+				addi $a0, $t0, 1
+				syscall
 			
 				#propmt for length. 
 				jal displayLengthPrompt
@@ -62,10 +98,23 @@
 	li $v0, 10
 	syscall
 	
-	displayTitle:
+	displayRecPrompt:
+		li $v0, 4
+		la $a0, recNumPrompt
+		syscall
+	jr $ra
+	
+	displayTitle1:
 		#Displays title.
 		li $v0, 4
-		la $a0, areaPrompt
+		la $a0, areaPrompt1
+		syscall
+	jr $ra
+	
+	displayTitle2:
+		#Displays title.
+		li $v0, 4
+		la $a0, areaPrompt2
 		syscall
 	jr $ra
 	
@@ -101,3 +150,5 @@
 		mul.s $f12, $f1, $f2
 		syscall
 	jr $ra
+	
+
