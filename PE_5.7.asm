bcf20cbb230a1966c491b552ee2cd2ace6c2a37b
danielhcg <danihcg.23@gmail.com>
2020-07-24 20:46:38 -0700

    Made changes to pe 5.7, need to calculate average

--- PE_5.7.asm
+++ PE_5.7.asm
@@ -19,16 +19,24 @@
 	monthInYear: .word 12
 	
 	yearMessage: .asciiz "Year "
-	monthMessage: .asciiz "Month\n"
-	doneMessage1: .asciiz "Done1!\n"
+	monthMessage: .asciiz "Month "
+	totalMonths: .asciiz "Total Months: "
+	totalRainfall: .asciiz "\nTotal Rainfall: "
+	avgRainfall:   .asciiz "\nAverage Rainfall: "
+	doneMessage1: .asciiz "\nDone1!\n"
 	doneMessage2: .asciiz "Done2!\n"
 	newLine: .asciiz "\n"
-	colone: .asciiz ": "
+	colon: .asciiz ": "
 	
+	zeroAsFloat: .float 0.0
+	runningTotal: .float 0.0
 
 .text
 
 	main: 
+	
+		lwc1 $f1, zeroAsFloat
+		lwc1 $f2, runningTotal
 	
 		# $t0 = years
 		# $t1 = outerIncrementor
@@ -56,12 +64,19 @@
 		#moving user inputted value for years into register t0
 		move $t0, $v0
 		
+
+		
 		while1: 
 		
 			#branch to exit if outerIncrementor is greater than hours
  			bgt $t1, $t0, exit1
  			
  				#loop body
+ 				
+ 				#display new line
+				li $v0, 4
+				la $a0, newLine
+				syscall
  				
  				#display year message
 				li $v0, 4  
@@ -70,10 +85,13 @@
 				
 				#dispay year iteration number
 				li $v0, 1
-				add $t1, $zero, $t1
+				add $a0, $zero, $t1
 				syscall
 				
 				#display new line
+				li $v0, 4
+				la $a0, newLine
+				syscall
 				
 				lw $t3, innerIncrementor
 				
@@ -82,9 +100,29 @@
 					bgt $t3, $t2, exit2
 					
 						#loop body
+						
+						#prompt user to enter rain fall for month
 						li $v0, 4
 						la $a0, monthMessage
 						syscall
+						
+						#display inner loop iteration number
+						li $v0, 1
+						add $a0, $zero, $t3
+						syscall
+						
+						#display a colon 
+						li $v0, 4
+						la $a0, colon
+						syscall
+						
+						#read user input for month, stored in $f0 
+						li $v0, 6 #code to read float: 6
+						syscall
+						
+						#add user input to running total 
+						add.s $f2, $f2, $f0 
+						
 					
 					addi $t3, $t3, 1
 					
@@ -104,6 +142,42 @@
 		
 		exit1: 
 		
+			#display total number of months message
+			li $v0, 4
+			la $a0, totalMonths
+			syscall
+			
+			#display total number of months
+			li $v0, 1
+			mul $a0, $t0, $t2
+			syscall
+			
+			#display total rain fall message
+			li $v0, 4
+			la $a0, totalRainfall
+			syscall
+			
+			#display running total number 
+			li $v0, 2  #code to print a float: 2
+			add.s $f12, $f2, $f1
+			syscall 
+			
+			
+			#display average rainfall message
+			li $v0, 4
+			la $a0, avgRainfall
+			syscall
+			
+			#display average rainfall
+			#running total / number of months 
+			
+			mul $t4, $t0, $t2
+			
+			
+			li $v0, 2
+			div.s $f12, $f2, $t2
+			
+			#display outerloop done message
 			li $v0, 4
 			la $a0, doneMessage1
 			syscall
